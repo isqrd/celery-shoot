6a9f8dc0d9f90f648833cf50828270c715995f95
Nathan Muir <ndmuir@gmail.com>
2015-08-09 13:32:21 +1000

    Clean up files published to NPM

--- meteor/package.js
+++ meteor/package.js
@@ -6,19 +6,19 @@ var repository = 'https://github.com/3stack-software/celery-shoot';
 
 Package.describe({
   name: packageName,
-  version: '4.0.1',
+  version: '4.0.3',
   summary: 'Connect Meteor to a python task server',
   git: repository,
-  documentation: 'README.md'
+  documentation: '../README.md'
 });
 
 Npm.depends({
-  "celery-shoot": '4.0.0'
+  "celery-shoot": '4.0.3'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.1.0.2');
 
   api.export('CeleryClient', 'server');
-  api.addFiles(['meteor/export.js'],'server');
+  api.addFiles(['export.js'],'server');
 });
