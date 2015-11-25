0230331c8d86aa43d78c40d15b80ded8d93f6eda
Nathan Muir <ndmuir@gmail.com>
2015-11-25 20:12:05 +1000

    Bump to version 4.20

--- meteor/package.js
+++ meteor/package.js
@@ -6,18 +6,18 @@ var repository = 'https://github.com/3stack-software/celery-shoot';
 
 Package.describe({
   name: packageName,
-  version: '4.1.0',
+  version: '4.2.0',
   summary: 'Connect Meteor to a python task server',
   git: repository,
   documentation: '../README.md'
 });
 
 Npm.depends({
-  "celery-shoot": "4.1.0"
+  "celery-shoot": "4.2.0"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.1.0.2');
+  api.versionsFrom('METEOR@1.2');
 
   api.export('CeleryClient', 'server');
   api.addFiles(['export.js'],'server');
