062b4c3120f590bb7bf9d8b2678633ec18b95fec
Nathan Muir <nathan.muir@3stack.com>
2018-11-07 17:20:00 +1000

    change result consumer to async iterator

--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,67 @@
 import Promise from 'bluebird';
 
+function defer() {
+  let resolve, reject;
+  const promise = new Promise((a, b) => {
+    resolve = a;
+    reject = b;
+  });
+  return {
+    resolve,
+    reject,
+    promise,
+  };
+}
+
+class AsyncChannel {
+  constructor() {
+    this.done = false; // when we're done, there's no future reads available.. just the buffer
+    this.buffer = []; // only when we have more data than waiters.
+    this.waiters = []; // queued/eager calls to `next()` that we don't have data for
+  }
+
+  push(value) {
+    if (this.done) {
+      return;
+    }
+    // give the next waiter the message
+    if (this.waiters.length) {
+      // assert this.buffer.length === 0
+      const waiter = this.waiters.shift();
+      waiter.resolve({ done: false, value });
+      return;
+    }
+    // no waiters, lets buffer the message for later
+    this.buffer.push(value);
+  }
+
+  close() {
+    if (this.done) {
+      return;
+    }
+    this.done = true;
+    while (this.waiters.length) {
+      // assert this.buffer.length === 0
+      const waiter = this.waiters.shift();
+      waiter.resolve({ done: true });
+    }
+  }
+
+  async next() {
+    if (this.buffer.length) {
+      // assert this.waiter.length === 0
+      const value = this.buffer.shift(); // pop(0)
+      return { done: false, value };
+    }
+    if (this.done) {
+      return { done: true };
+    }
+    const waiter = defer();
+    this.waiters.push(waiter);
+    return waiter.promise;
+  }
+}
+
 export class CeleryResultError extends Error {
   constructor(status, result, traceback, ...params) {
     super(...params);
@@ -35,78 +97,73 @@ export default class Backend {
     this._exchangeAsserted = false;
   }
 
-  assertExchange() {
+  async assertExchange() {
     // celery will assert the exchange, before sending a result
     // however, we cannot bind our queue until it exists
     if (this._exchangeAsserted) {
-      return;
+      return null;
     }
     this._exchangeAsserted = true;
     // TODO
+    return null;
   }
 
-  getResult(taskId) {
-    // eslint-disable-next-line one-var,one-var-declaration-per-line
-    let resolve, reject;
-    const onResult = new Promise((innerResolve, innerReject) => {
-      resolve = innerResolve;
-      reject = innerReject;
-    });
+  async getResult(taskId) {
+    const { results, close } = await this.consumeResults(taskId);
 
-    const handleMessage = msg => {
-      if (msg == null) {
-        reject(new Error('Consumer cancelled by RabbitMQ'));
-      }
-      const { content } = msg;
-      const data = JSON.parse(content.toString());
-      const status = data.status.toLowerCase();
-
-      if (
-        status === 'failure' ||
-        status === 'revoked' ||
-        status === 'ignored'
-      ) {
-        reject(
-          new CeleryResultError(
+    try {
+      // TODO support track-started
+      // TODO use await Promise.race(Promise.delay(ms).then(()=> throw new reason), ...)
+      let { done, value = null } = await results.next();
+      while (!done) {
+        const data = JSON.parse(value.content.toString());
+        const status = data.status.toLowerCase();
+        if (
+          status === 'failure' ||
+          status === 'revoked' ||
+          status === 'ignored'
+        ) {
+          throw new CeleryResultError(
             data.status,
             data.result,
             data.traceback,
             `Task ${taskId} ${status}`,
-          ),
-        );
-      } else if (status === 'success') {
-        resolve(data.result);
-      }
-    };
+          );
+        } else if (status === 'success') {
+          return data.result;
+        }
 
-    return Promise.using(
-      this.consumeResults(taskId, handleMessage),
-      () => onResult,
-    );
+        ({ done, value = null } = await results.next());
+      }
+    } finally {
+      await close();
+    }
   }
 
-  consumeResults(taskId, onMessage) {
+  async consumeResults(taskId) {
     const { channel, queueOptions, exchange } = this;
     const resultsQueue = taskId.replace(/-/g, '');
-    let close = null;
-
-    // ensure the results exchange is configured
-    this.assertExchange();
-    // to be used with `Promise.using`, cleans up consumerTag when done
-    return Promise.all([
+    const results = new AsyncChannel();
+    const [, , , { consumerTag }] = await Promise.all([
+      // as these are rpc calls over the one channel, they will complete serially
+      this.assertExchange(),
       channel.assertQueue(resultsQueue, queueOptions),
       channel.bindQueue(resultsQueue, exchange, '#'),
-      channel.consume(resultsQueue, onMessage),
-    ])
-      .spread((assertOk, bindOk, { consumerTag }) => {
-        close = consumerTag;
-        // no return
-      })
-      .disposer(() => {
-        if (close != null) {
-          return channel.cancel(close);
+      channel.consume(resultsQueue, msg => {
+        if (msg == null) {
+          results.close();
+        } else {
+          results.push(msg);
         }
-        return null;
-      });
+      }),
+    ]);
+
+    return {
+      results,
+      async close() {
+        results.close();
+        return channel.cancel(consumerTag);
+      },
+    };
   }
 }
